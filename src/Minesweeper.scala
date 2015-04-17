94115dcdbe5763e79515767ad7be975658e1d46a
Leonardo Lucena <leonardo.lucena@escolar.ifrn.edu.br>
2015-04-17 14:01:03 -0300

    Refactoring

--- src/Minesweeper.scala
+++ src/Minesweeper.scala
@@ -1,82 +1,67 @@
 import java.util.Random
+import Game1._
+import scala.annotation.tailrec
+sealed abstract class Cell {
+  val shown: Boolean
+}
+case class Bomb(shown: Boolean = false) extends Cell
+case class Empty(shown: Boolean = false) extends Cell
+case class Hint(shown: Boolean = false, val x: Int) extends Cell
 
-abstract class Cell(shownc: Boolean) {
-  val shown: Boolean = shownc
+object Game1 {
+  type Board = List[List[Cell]]
+  implicit class BoardUpdate(board: Board) {
+    def update(x: Int, y: Int, value: Cell) = board.updated(x, board(x).updated(y, value))
+  }
 }
 
-case class Bomb(override val shown: Boolean) extends Cell(shown)
-case class Empty(override val shown: Boolean) extends Cell(shown)
-case class Hint(override val shown: Boolean, val xc: Int) extends Cell(shown) {
-  val x = xc
-}
+class Game(val rows: Int, val columns: Int, val bombs: Int, boardc: Option[Board] = None) {
+  val board = boardc.getOrElse(initBoard)
 
-class Game(rowsc: Int, columnsc: Int, bombsc: Int, boardc: List[List[Cell]] = Nil) {
-  val rows = rowsc
-  val columns = columnsc
-  val bombs = bombsc
-  val board = if (boardc != Nil) boardc else initBoard()
-
-  private def initBoard(): List[List[Cell]] = {
-    val board1 = List.tabulate(rows)(_ => List.tabulate(columns)(_ => new Empty(false)))
+  lazy val initBoard = {
+    val board1 = List.fill(rows, columns) { Empty() }
     val board2 = initBombs(bombs, board1)
     initHints(board2, rows, columns)
   }
 
   //bombs
-  private def initBombs(quantity: Int, board: List[List[Cell]]): List[List[Cell]] = {
-    val newboard = tryToPutBomb(board)
-    if (quantity > 1) {
-      initBombs(quantity - 1, newboard)
+  @tailrec private def initBombs(quantity: Int, board: Board): Board = {
+    if (quantity > 0) {
+      initBombs(quantity - 1, tryToPutBomb(board))
     } else {
-      newboard
+      board
     }
   }
 
-  private def putBomb(x: Int, y: Int, board: List[List[Cell]]): List[List[Cell]] = {
-    board.updated(x, board(x).updated(y, Bomb(false)))
-  }
-
-  private def tryToPutBomb(board: List[List[Cell]]): List[List[Cell]] = {
-    val x = new Random().nextInt(rows)
-    val y = new Random().nextInt(columns)
-    val t: Cell = board(x)(y)
-    t match {
+  @tailrec private def tryToPutBomb(board: Board): Board = {
+    val r = new Random
+    val x = r.nextInt(rows)
+    val y = r.nextInt(columns)
+    board(x)(y) match {
       case Bomb(_) => tryToPutBomb(board)
-      case _ => putBomb(x, y, board)
+      case _ => board.update(x, y, Bomb())
     }
   }
   //end of bombs
 
   //hints
-  private def initHints(board: List[List[Cell]], x: Int, y: Int): List[List[Cell]] = {
-    List.tabulate(x, y)((i, j) => transformIntoHint(board, i, j))
+  private def initHints(board: Board, x: Int, y: Int) =
+    List.tabulate(x, y) { transformIntoHint(board, _, _) }
+
+  private def transformIntoHint(boardWithBombs: Board, x: Int, y: Int) = boardWithBombs(x)(y) match {
+    case Bomb(b) => Bomb(b)
+    case _ => initHint(boardWithBombs, x, y)
   }
 
-  private def transformIntoHint(boardWithBombs: List[List[Cell]], x: Int, y: Int): Cell = {
-    val cell = boardWithBombs(x)(y)
-    cell match {
-      case Bomb(b) => Bomb(b)
-      case _ => initHint(boardWithBombs, x, y)
-    }
-  }
-
-  private def initHint(boardWithBombs: List[List[Cell]], x: Int, y: Int): Cell = {
-    def countBomb(cell: Cell): Int = {
-      cell match {
+  private def initHint(boardWithBombs: Board, x: Int, y: Int) = {
+    val neighborCells =
+      for (i <- -1 to 1; j <- -1 to 1 if i != 0 || j != 0) yield getCellOnBoard(boardWithBombs)(x + i, y + j)
+    val hintValue = {
+      neighborCells.flatten.map {
         case Bomb(_) => 1
         case _ => 0
-      }
+      }.sum
     }
-
-    val neighborCells = getCellOnBoard(boardWithBombs, x - 1, y - 1) ::
-                        getCellOnBoard(boardWithBombs, x, y - 1) ::
-                        getCellOnBoard(boardWithBombs, x + 1, y - 1) ::
-                        getCellOnBoard(boardWithBombs, x - 1, y) ::
-                        getCellOnBoard(boardWithBombs, x + 1, y) ::
-                        getCellOnBoard(boardWithBombs, x - 1, y + 1) ::
-                        getCellOnBoard(boardWithBombs, x, y + 1) ::
-                        getCellOnBoard(boardWithBombs, x + 1, y + 1) :: Nil
-    val hintValue = (neighborCells.map(countBomb)).foldLeft(0)(_ + _)
     hintValue match {
       case 0 => Empty(false)
       case _ => Hint(false, hintValue)
@@ -85,78 +70,49 @@ class Game(rowsc: Int, columnsc: Int, bombsc: Int, boardc: List[List[Cell]] = Ni
   //end of hints
 
   //game behavior
-  def showCell(tuple: (Int, Int)): Game = {
-    val x = tuple._1
-    val y = tuple._2
-    val cell = board(x)(y)
-    cell match {
+  def showCell(x: Int, y: Int): Game = {
+    board(x)(y) match {
       case Empty(false) => showNeighborCells(x, y)
       case Bomb(false) => {
-        val newboard = board.updated(x, board(x).updated(y, Bomb(true)))
-        new Game(rows, columns, bombs, newboard)
+        val newboard = board.update(x, y, Bomb(true))
+        new Game(rows, columns, bombs, Some(newboard))
       }
       case Hint(false, hint) => {
-        val newboard = board.updated(x, board(x).updated(y, Hint(true, hint)))
-        new Game(rows, columns, bombs, newboard)
+        val newboard = board.update(x, y, Hint(true, hint))
+        new Game(rows, columns, bombs, Some(newboard))
       }
-      case _ => new Game(rows, columns, bombs, board)
+      case _ => this
     }
   }
 
-  private def showNeighborCells(x: Int, y: Int): Game = {
-    def getPosition(board: List[List[Cell]], x: Int, y: Int): List[(Int, Int)] = {
-      if (contains(x, y)) {
-        List(Tuple2(x, y))
-      } else {
-        Nil
-      }
-    }
+  private def showNeighborCells(x: Int, y: Int): Game = {  
+    def getPosition(board: Board, x: Int, y: Int) =
+      if (contains(x, y)) Some((x, y)) else None
 
-    val newboard = board.updated(x, board(x).updated(y, Empty(true)))
-    val newGame = new Game(rows, columns, bombs, newboard)
-    val neighborPositions: List[(Int, Int)] = getPosition(board, x - 1, y - 1) ++
-                                              getPosition(board, x, y - 1) ++
-                                              getPosition(board, x + 1, y - 1) ++
-                                              getPosition(board, x - 1, y) ++
-                                              getPosition(board, x + 1, y) ++
-                                              getPosition(board, x - 1, y + 1) ++
-                                              getPosition(board, x, y + 1) ++
-                                              getPosition(board, x + 1, y + 1) ++ Nil
-    neighborPositions.foldLeft(newGame)((game, tuple) => game.showCell(tuple))
+    val newboard = board.update(x, y, Empty(true))
+    val newGame = new Game(rows, columns, bombs, Some(newboard))
+    val neighborPositions =
+      for (i <- -1 to 1; j <- -1 to 1 if i != 0 || j != 0) yield getPosition(board, x + i, y + j)
+    neighborPositions.flatten.foldLeft(newGame)((game, tuple) => { val (x, y) = tuple; game.showCell(x, y) })
   }
 
-  def hasOnlyBombs(): Boolean = {
-    !board.flatten.exists(cell => cell match {
-      case Empty(false) => true
-      case Hint(false, _) => true
-      case _ => false
-    })
+  def hasOnlyBombs = !board.flatten.exists {
+    case Empty(false) | Hint(false, _) => true
+    case _ => false
   }
 
-  def hasActiveBomb(): Boolean = {
-    board.flatten.exists(cell => cell match {
-      case Bomb(true) => true
-      case _ => false
-    })
+  def hasActiveBomb = board.flatten.exists {
+    case Bomb(true) => true
+    case _ => false
   }
 
-  def contains(x: Int, y: Int): Boolean = {
+  def contains(x: Int, y: Int) = {
     x >= 0 && x < rows && y >= 0 && y < columns
   }
 
-  def getCell(x: Int, y: Int): Cell = {
-    if (contains(x, y)) {
-      board(x)(y)
-    } else {
-      null
-    }
-  }
-  
-  def getCellOnBoard(board: List[List[Cell]], x: Int, y: Int): Cell = {
-    if (contains(x, y)) {
-      board(x)(y)
-    } else {
-      null
-    }
+  val getCell = getCellOnBoard(board) _
+
+  def getCellOnBoard(board: Board)(x: Int, y: Int) = {
+    if (contains(x, y)) Some(board(x)(y)) else None
   }
 }
